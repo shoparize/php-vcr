8ed0828e7206822526a4071c2265a98af888aa72
akinaksu <1243tamam>
2021-08-30 18:38:03 +0300

    issue; https://github.com/php-vcr/php-vcr/issues/309 applied; https://github.com/WeChalet/php-vcr/commit/86949921c1deea5d27913a518b29b290ddcaa6af

--- src/VCR/Util/CurlHelper.php
+++ src/VCR/Util/CurlHelper.php
@@ -36,6 +36,7 @@ class CurlHelper
         \CURLINFO_CONTENT_LENGTH_DOWNLOAD => 'download_content_length',
         \CURLINFO_CONTENT_LENGTH_UPLOAD => 'upload_content_length',
         \CURLINFO_CONTENT_TYPE => 'content_type',
+        \CURLINFO_APPCONNECT_TIME => 'appconnect_time',
     ];
 
     /**
@@ -112,6 +113,7 @@ class CurlHelper
                 $info = mb_strlen(HttpUtil::formatAsStatusWithHeadersString($response), 'ISO-8859-1');
                 break;
             case \CURLPROXY_HTTPS:
+            case \CURLINFO_APPCONNECT_TIME:
                 $info = '';
                 break;
             default:
